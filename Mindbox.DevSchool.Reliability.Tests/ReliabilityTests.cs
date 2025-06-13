881e45504d3b31893e8de0f94af23964d6328cad
Ilshat Mukhamedyarov <mukhamedyarov@mindbox.cloud>
2025-06-13 22:59:38 +0300

    init

---
+++ Mindbox.DevSchool.Reliability.Tests/ReliabilityTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace Mindbox.DevSchool.Reliability.Tests;
+
+[TestClass]
+public sealed class ReliabilityTests
+{
+	[TestMethod]
+	public async Task ThreadStarvation_Test()
+	{
+		using var httpClient = new HttpClient();
+		httpClient.BaseAddress = new Uri("http://localhost:5013");
+		httpClient.Timeout = TimeSpan.FromSeconds(2);
+
+		var makeApiCallTasks = Enumerable.Range(0, 999)
+			.Select(_ => httpClient.GetAsync("/weatherForecast/1"))
+			.ToArray();
+
+		var responses = await Task.WhenAll(makeApiCallTasks);
+
+		foreach (var httpResponseMessage in responses)
+		{
+			Assert.AreEqual(HttpStatusCode.OK, httpResponseMessage.StatusCode);
+		}
+	}
+}
