7de257e45bddf17949a6471e72c1b818645e14bc
Ilshat Mukhamedyarov <mukhamedyarov@mindbox.cloud>
2025-06-14 13:35:26 +0300

    Update ReliabilityTests.cs

--- Mindbox.DevSchool.Reliability.Tests/ReliabilityTests.cs
+++ Mindbox.DevSchool.Reliability.Tests/ReliabilityTests.cs
@@ -6,14 +6,14 @@ namespace Mindbox.DevSchool.Reliability.Tests;
 public sealed class ReliabilityTests
 {
 	[TestMethod]
-	public async Task ThreadStarvation()
+	public async Task Reliability()
 	{
 		using var httpClient = new HttpClient();
 		httpClient.BaseAddress = new Uri("http://localhost:5013");
 		httpClient.Timeout = TimeSpan.FromSeconds(3);
 
 		var makeApiCallTasks = Enumerable.Range(0, 4)
-			.Select(_ => httpClient.GetAsync("async/weatherForecast/c0f4ac08-eafc-4fdb-91f8-fb39dda1d216"))
+			.Select(_ => httpClient.GetAsync("weatherForecast/c0f4ac08-eafc-4fdb-91f8-fb39dda1d216"))
 			.ToArray();
 
 		var responses = await Task.WhenAll(makeApiCallTasks);
@@ -25,23 +25,30 @@ public sealed class ReliabilityTests
 	}
 
 	[TestMethod]
-	public async Task ThreadStarvation_2()
+	public async Task Reliability2()
 	{
+		using var timeoutHttpClient = new HttpClient();
+		timeoutHttpClient.BaseAddress = new Uri("http://localhost:5013");
+		timeoutHttpClient.Timeout = TimeSpan.FromMilliseconds(1);
+		
+		var timeoutTasks = Enumerable.Range(0, 1000)
+			.Select(_ => timeoutHttpClient.GetAsync("ct/async/weatherForecast/c0f4ac08-eafc-4fdb-91f8-fb39dda1d216"))
+			.ToArray();
+
+		try
+		{
+			await Task.WhenAll(timeoutTasks);
+		}
+		catch
+		{
+			// ignore
+		}
+
+		await Task.Delay(TimeSpan.FromSeconds(1));
+		
 		using var httpClient = new HttpClient();
 		httpClient.BaseAddress = new Uri("http://localhost:5013");
 		httpClient.Timeout = TimeSpan.FromSeconds(3);
-		
-		using var tokenSource = new CancellationTokenSource();
-
-		Enumerable.Range(0, 100)
-			.Select(_ => httpClient.GetAsync("ct/async/weatherForecast/c0f4ac08-eafc-4fdb-91f8-fb39dda1d216", tokenSource.Token))
-			.ToArray();
-
-		await Task.Delay(TimeSpan.FromSeconds(1));
-
-		await tokenSource.CancelAsync();
-		
-		await Task.Delay(TimeSpan.FromSeconds(1));
 		
 		var apiCallTasks = Enumerable.Range(0, 4)
 			.Select(_ => httpClient.GetAsync("ct/async/weatherForecast/c0f4ac08-eafc-4fdb-91f8-fb39dda1d216"))
